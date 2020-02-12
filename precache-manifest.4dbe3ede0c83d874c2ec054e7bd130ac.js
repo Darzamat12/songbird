d869a20e39d09f17492e8012d1224ea1a15f4673
unknown <ogurezslava@gmai.com>
2020-02-13 01:15:29 +0300

    Updates

---
+++ precache-manifest.4dbe3ede0c83d874c2ec054e7bd130ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55dc8ac6a3cec2756670398d3c39d528",
+    "url": "/mybirds/index.html"
+  },
+  {
+    "revision": "6d02b945b989554e42f9",
+    "url": "/mybirds/static/css/main.3bdc9985.chunk.css"
+  },
+  {
+    "revision": "4741840df72d25ca39c8",
+    "url": "/mybirds/static/js/2.4b17bdac.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mybirds/static/js/2.4b17bdac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d02b945b989554e42f9",
+    "url": "/mybirds/static/js/main.dfbaa64d.chunk.js"
+  },
+  {
+    "revision": "09adb4c15b94acc5552d",
+    "url": "/mybirds/static/js/runtime-main.a836f767.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/mybirds/static/media/logo.ee7cd8ed.svg"
+  }
+]);
